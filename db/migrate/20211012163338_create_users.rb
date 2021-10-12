015995bf727d117a401944fd1b81f5f2cae82177
John Lonetree <john.lonetree@kin.com>
2021-10-12 11:38:07 -0500

    Added User model, controller and views

---
+++ db/migrate/20211012163338_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :image
+      t.string :faction
+      t.integer :credits
+      t.string :first_name
+      t.string :last_name
+      t.date :birthdate
+
+      t.timestamps
+    end
+  end
+end
